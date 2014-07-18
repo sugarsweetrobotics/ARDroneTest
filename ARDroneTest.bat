13f5b75751eb7ddd54bcbc9e6691e977f7bf3933
ogata-lab <tech@ogata-lab.jp>
2014-07-18 11:40:44 +0900

    Modify Windows Bat script

--- ARDroneTest.bat
+++ ARDroneTest.bat
@@ -1,5 +1,6 @@
-set CLASSPATH=.;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
+set RTM_JAVA_ROOT=.
+set CLASSPATH=bin;%RTM_JAVA_ROOT%\jar\OpenRTM-aist-1.1.0.jar;%RTM_JAVA_ROOT%\jar\commons-cli-1.1.jar
 cd /d %~dp0
-cd bin
+@rem cd bin
 java ARDroneTestComp -f rtc.conf %*
 pause;
